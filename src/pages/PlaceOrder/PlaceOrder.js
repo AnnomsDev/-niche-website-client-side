c5d43b4bac3d0e26900755ae9f9e96c0de983dae
AnnomsDev <foysalbinnour@gmail.com>
2021-11-11 21:41:43 +0600

    place order functionality implemented

--- src/pages/PlaceOrder/PlaceOrder.js
+++ src/pages/PlaceOrder/PlaceOrder.js
@@ -1,20 +1,45 @@
-import { Button, Container, Grid, Typography } from '@mui/material';
+import { Alert, Button, Container, Grid, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import useAuth from '../../Hook/useAuth';
 
 const PlaceOrder = () => {
     const { id } = useParams()
+    const { user } = useAuth()
     const [product, setProduct] = useState({})
+    const [isOrderPlaced, setIsOrderPlace] = useState(false)
     console.log(id)
 
-    const { title, img, details, camera, flight_time, price } = product
+    const { _id, title, img, details, camera, flight_time, price } = product
 
     useEffect(() => {
         axios.get(`http://localhost:5000/products/${id}`)
             .then(res => setProduct(res.data))
             .catch(err => console.log('Got an error: ', err))
     }, [id])
+
+    const handlePlaceOrder = () => {
+        const email = user.email
+        const productId = _id
+        const productName = title
+
+        const order = { email, productId, productName, price }
+
+        axios.post('http://localhost:5000/orders', order)
+            .then(res => {
+                if (res.data.insertedId) {
+                    setIsOrderPlace(true)
+                }
+            })
+            .catch(err => {
+                console.log('Got an error placing order', err)
+                alert('Something went wrong!!, Please try again')
+
+            })
+
+
+    }
 
     return (
         <Container
@@ -50,9 +75,12 @@ const PlaceOrder = () => {
                     <Typography variant='h5' sx={{ fontWeight: 700, mt: '2em' }}>
                         Price: <span style={{ color: '#1565C0' }}>$</span>{price}
                     </Typography>
-                    <Button variant='outlined' sx={{ fontWeight: 700, my: '1em' }}>Purchess</Button>
-
-
+                    <Button
+                        variant='outlined'
+                        sx={{ fontWeight: 700, my: '1em' }}
+                        onClick={handlePlaceOrder}
+                    >Place Order</Button>
+                    {isOrderPlaced && <Alert severity="success">Order placed Successfully</Alert>}
 
                 </Grid>
 
